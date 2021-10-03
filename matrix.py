f373ef774a077ba843844e063b62aec936f1d21a
Katherine Tweeddale <72230058+KatT92@users.noreply.github.com>
2021-10-03 20:35:34 +0100

    Update matrix.py

--- matrix.py
+++ matrix.py
@@ -1,14 +1,16 @@
 class Matrix:
     def __init__(self, matrix_string):
         self.matrix = [[int(i) for i in r.split()] for r in matrix_string.split("\n")]
-
+		
     def row(self, index):
-        return self.matrix[index]
+        return self.matrix[index - 1]
 
     def column(self, index):
-        column = list(zip(*self.matrix[index]))
-        return column
+        return [j[index - 1] for j in self.matrix]
 
+        
+
+          
 
 
 
